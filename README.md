e355eca7ac1fa7221a77a7e4709b397f44e2352f
yyz <365939114@qq.com>
2022-05-04 15:15:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## SpringBoot + Vue 的物流管理系统
+
+#### 主要框架
+- SpringBoot
+- SpringData
+- SpringSecurity
+- Vue2
+
+
+
+# 源码完整版+QQ：365939114 咨询获取，可一对一专业指定需求，个性化制定代码。
---
+++ webapp/src/layout/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer :style="{ textAlign: 'center', letterSpacing: '1px' }">
+    物流管理系统 ©2021 Yang Yezhuang
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
